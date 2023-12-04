2781699b3d5332fac1f415bcf750ec7e24546193
Florin Miron <mironflorin123@yahoo.com>
2023-12-04 19:37:06 +0200

    create functionality for scores modal fade in effect

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const rulesModal = document.querySelector('.rules-modal');
 const rulesBtn = document.querySelector('.rules-btn');
 const closeRulesBtn = document.getElementById('close-rules-btn');
+const scoresBtn = document.querySelector('.scores-btn');
+const scoresModal = document.querySelector('.scores-modal');
+const closeScoresBtn = document.getElementById('close-scores-btn');
 
-function showRulesModal () {
+function showRulesModal() {
     rulesModal.classList.add('rules-modal-visible');
 }
 
@@ -10,5 +13,15 @@ function hideRulesModal() {
     rulesModal.classList.remove('rules-modal-visible');
 }
 
+function showScoresModal() {
+    scoresModal.classList.add('scores-modal-visible');
+}
+
+function hideScoresModal() {
+    scoresModal.classList.remove('scores-modal-visible');
+}
+
 rulesBtn.addEventListener('click', showRulesModal);
 closeRulesBtn.addEventListener('click', hideRulesModal);
+scoresBtn.addEventListener('click', showScoresModal);
+closeScoresBtn.addEventListener('click', hideScoresModal);
