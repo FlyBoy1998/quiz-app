da26dafc5c97f092daae7d569de2d149a9bc7931
Florin Miron <mironflorin123@yahoo.com>
2023-12-05 11:27:15 +0200

    create functionality for start button

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const closeRulesBtn = document.getElementById('close-rules-btn');
 const scoresBtn = document.querySelector('.scores-btn');
 const scoresModal = document.querySelector('.scores-modal');
 const closeScoresBtn = document.getElementById('close-scores-btn');
+const startBtn = document.querySelector('.start-btn');
+const mainMenuContainer = document.querySelector('.main-menu-container');
+const difficultyLevelContainer = document.querySelector('.difficulty-level-container');
+const difficultyLevelBtns = document.querySelectorAll('.difficulty-level-btn');
 
 function showRulesModal() {
     rulesModal.classList.add('rules-modal-visible');
@@ -21,7 +25,13 @@ function hideScoresModal() {
     scoresModal.classList.remove('scores-modal-visible');
 }
 
+function start() {
+    mainMenuContainer.classList.add('hidden');
+    difficultyLevelContainer.classList.remove('hidden');
+}
+
 rulesBtn.addEventListener('click', showRulesModal);
 closeRulesBtn.addEventListener('click', hideRulesModal);
 scoresBtn.addEventListener('click', showScoresModal);
 closeScoresBtn.addEventListener('click', hideScoresModal);
+startBtn.addEventListener('click', start);
